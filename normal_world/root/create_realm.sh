009b3fee1b0fbfc929e0acf6ba8abb9e86bfc36b
SinaAb7 <sinaabdollahip@gmail.com>
2024-04-01 19:20:20 +0100

    Added more scripts to support shared folder creation

--- normal_world/root/create_realm.sh
+++ normal_world/root/create_realm.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+folder_name="shared_with_realm"
+if [ ! -d "$folder_name" ]; then
+    mkdir "$folder_name"
+fi
 screen lkvm run --realm -c 1 -m 300 -k /realm/Image -d /realm/realm-fs.ext4 \
 --9p /root/shared_with_realm,sh -p earlycon  --irqchip=gicv3 --disable-sve
