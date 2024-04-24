b3dd69b820d0ad98c6890577bf10f4200030aa9e
Sina Abdollahi <sinaabdollahip@gmail.com>
2024-04-24 15:02:06 +0100

    Delete normal_world/create_NW_VM.sh

--- normal_world/create_NW_VM.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-folder_name="shared_with_realm"
-if [ ! -d "$folder_name" ]; then
-    mkdir "$folder_name"
-fi
-screen lkvm run -c 1 -m 300 -k /realm/Image -i /realm/realm-fs.cpio \
---9p /root/shared_with_realm,sh --irqchip=gicv3 --disable-sve
